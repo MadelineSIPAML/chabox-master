46cf0077903a170e98b01ed5e19fb06ecaa9d0dc
Tatiana Toledo <tatiana.toledo550@gmail.com>
2026-01-20 11:55:00 -0300

    Fix PORT configuration for Render

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8000))
     app.run(debug=False, host='0.0.0.0', port=port)
